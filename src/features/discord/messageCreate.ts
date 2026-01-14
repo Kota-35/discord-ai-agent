f9fd504cf87383704560b7032d474bc2a4e59ab2
Kota-35 <ishikuro6.2@gmail.com>
2026-01-14 13:31:21 +0900

    feat: botがメンションされていない場合には返信しない

--- src/features/discord/messageCreate.ts
+++ src/features/discord/messageCreate.ts
@@ -2,12 +2,19 @@ import { gourmetSearchAgent } from '@/features/mastra/agents/gourmetSearchAgent'
 import { discordClient } from '@/libs/discord';
 
 discordClient.on('messageCreate', async (message) => {
-  console.info('✅ messageCreate');
-
   // メッセージの送信者がbotなら無視
   if (message.author.bot) {
     return;
   }
+
+  // botがメンションされていない場合には無視
+  if (!message.mentions.has(discordClient.user!)) {
+    return;
+  }
+
+  console.info('✅ messageCreate');
+  console.info(`${message.author.displayName}`)
+  console.info(`${message.content}`)
 
   const response = await gourmetSearchAgent.generate(message.content);
 
